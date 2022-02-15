e16511bbeb740f8264fd3bb4e0d26c1e322ccf80
Dourado <a@s.com>
2022-02-15 13:40:58 -0300

    [refactor] Refactor the colors name of search_screen file and change the local of the git-class repository

--- projects/academy/tests/features/ppt-git/search_screen.py
+++
@@ -1,8 +0,0 @@
-from projects.academy.utils.logger import logger
-
-logger.info('--- Search screen ---')
-
-logger.info('Search field - shape fill color : blue')
-logger.info('Search field  - shape outline color: blue')
-logger.info('Search icon color: black')
-logger.info('Background screen color: green ')
