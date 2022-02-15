79b25dc8936f6182791c3931fd5e7a5745333d8d
Dourado <a@s.com>
2022-02-15 13:52:19 -0300

    [refactor] Change the colors of search_screen file

--- projects/academy/git-class/search_screen.py
+++ projects/academy/git-class/search_screen.py
@@ -3,6 +3,6 @@ from projects.academy.utils.logger import logger
 logger.info('--- Search screen ---')
 
 logger.info('Search field - shape fill color : white')
-logger.info('Search field  - shape outline color: white')
+logger.info('Search field  - shape outline color: blue')
 logger.info('Search icon color: black')
 logger.info('Background screen color: white ')
