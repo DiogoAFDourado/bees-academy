ed09f547ef546ddcad6c0cf82f9c165bf6c08252
DiogoAFDourado <94140584+DiogoAFDourado@users.noreply.github.com>
2022-02-16 09:16:41 -0300

    alteracao na branch test (#3)
    Co-authored-by: Dourado <a@s.com>

--- projects/academy/git-class/search_screen.py
+++ projects/academy/git-class/search_screen.py
@@ -2,7 +2,7 @@ from projects.academy.utils.logger import logger
 
 logger.info('--- Search screen ---')
 
-logger.info('Search field - shape fill color : test')
+logger.info('Search field - shape fill color : test1')
 logger.info('Search field  - shape outline color: orange')
 logger.info('Search icon color: black')
 logger.info('Background screen color: yellow ')
