2fc4de5bec7a91911e1b478ebc64392e157f9423
DiogoAFDourado <94140584+DiogoAFDourado@users.noreply.github.com>
2022-02-16 09:35:12 -0300

    alteracao na branch B2 (#9)
    Co-authored-by: Dourado <a@s.com>

--- projects/academy/git-class/search_screen.py
+++ projects/academy/git-class/search_screen.py
@@ -2,7 +2,7 @@ from projects.academy.utils.logger import logger
 
 logger.info('--- Search screen ---')
 
-logger.info('Search field - shape fill color : white')
+logger.info('Search field - shape fill color : alteração feita pela B2')
 logger.info('Search field  - shape outline color: blue')
 logger.info('Search icon color: black')
 logger.info('Background screen color: white ')
