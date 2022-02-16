01beaf3f89bae6837d2272459ea5348d07637351
Dourado <a@s.com>
2022-02-16 09:15:12 -0300

    alteracao na branch your-branch-name

---
+++ projects/academy/git-class/test.py
@@ -0,0 +1,8 @@
+from projects.academy.utils.logger import logger
+
+logger.info('--- Search screen ---')
+
+logger.info('Search field - shape fill color : test')
+logger.info('Search field  - shape outline color: orange')
+logger.info('Search icon color: black')
+logger.info('Background screen color: yellow ')
