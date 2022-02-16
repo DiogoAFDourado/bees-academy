5cac69c447e111394c960aaf27016335fe33e86e
Dourado <a@s.com>
2022-02-16 09:39:45 -0300

    alteracao na branch B2

--- projects/academy/git-class/search_screen.py
+++ projects/academy/git-class/search_screen.py
@@ -3,6 +3,6 @@ from projects.academy.utils.logger import logger
 logger.info('--- Search screen ---')
 
 logger.info('Search field - shape fill color : alteração feita pela B2')
-logger.info('Search field  - shape outline color: blue')
+logger.info('Search field  - shape outline color: alteração feita pela B2')
 logger.info('Search icon color: black')
 logger.info('Background screen color: white ')
