edd516626edc4639d8a9d416b2483233afa437a3
DiogoAFDourado <94140584+DiogoAFDourado@users.noreply.github.com>
2022-02-16 09:41:43 -0300

    alteracao na branch B1 (#10)
    Co-authored-by: Dourado <a@s.com>

--- projects/academy/git-class/test.py
+++ projects/academy/git-class/test.py
@@ -3,6 +3,6 @@ from projects.academy.utils.logger import logger
 logger.info('--- Search screen ---')
 
 logger.info('Search field - shape fill color : alteração pela B1')
-logger.info('Search field  - shape outline color: blue')
+logger.info('Search field  - shape outline color: alteração pela B1')
 logger.info('Search icon color: black')
 logger.info('Background screen color: white ')
